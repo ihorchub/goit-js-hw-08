8451a3677e869c928146d170113ad303bc2c8919
Ihor Chubov <ihor.chub@gmail.com>
2022-10-09 19:17:15 +0300

    task #3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const dataParce = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+let formData = {};
+let email = '';
+let message = '';
+
+function addToLocalStorage() {
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function handleInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  addToLocalStorage();
+}
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  console.log(formData);
+  evt.currentTarget.reset();
+  localStorage.clear();
+}
+
+try {
+  email = dataParce.email;
+  message = dataParce.message;
+  formData = dataParce;
+} catch (error) {
+  console.error('Get state error: ', error.message);
+} finally {
+  if (email) {
+    feedbackForm.elements.email.value = email;
+  } else {
+    feedbackForm.elements.email.value = '';
+  }
+  if (message) {
+    feedbackForm.elements.message.value = message;
+  } else {
+    feedbackForm.elements.message.value = '';
+  }
+}
+
+feedbackForm.addEventListener('input', throttle(handleInput, 500));
+feedbackForm.addEventListener('submit', handleSubmit);
